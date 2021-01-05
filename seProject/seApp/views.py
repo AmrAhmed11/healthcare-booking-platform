b7df86fd3db2d61ab71f1eb34872abfb127883ec
Loayelshall <Loay.elshall@icloud.com>
2021-01-05 12:50:28 +0200

    added view and urls for services manager(fees and timeslots) and staff manager(nurses)

--- seProject/seApp/views.py
+++ seProject/seApp/views.py
@@ -11,3 +11,15 @@ def appointment(request, app_id):
     app = {'id':4, 'date':'1/1/2022', 'state': 'Pending'}
     context = {'app': app}
     return render(request, 'seApp/appointment.html', context)
+
+
+def servicesManager(request):
+    services_list = {'fees':'100$', 'timeslots':[{'id':0,'slot':'2020/12/12'},{'id':1,'slot':'2020/12/12'},{'id':2,'slot':'2020/12/12'}] }
+    context = {'services_list': services_list}
+    return render(request, 'seApp/servicesManager.html', context)
+
+
+def staffManager(request):
+    staff_list = [{'id':0, 'name':'John', 'details': 'nurse'}, {'id':1, 'name':'John', 'details': 'nurse'},{'id':2, 'name':'John', 'details': 'nurse'},{'id':3, 'name':'John', 'details': 'nurse'}]
+    context = {'staff_list': staff_list}
+    return render(request, 'seApp/staffManager.html', context)
