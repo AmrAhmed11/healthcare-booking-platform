247df63689fb148da4a488ac151f90edb8b1b511
Omar Rashed <70315665+OmarRash@users.noreply.github.com>
2021-01-06 00:03:44 +0200

    models sqlite

--- seProject/seApp/models.py
+++ seProject/seApp/models.py
@@ -1,14 +1,47 @@
 from django.db import models
 from jsonfield import JSONField
-# Create your models here.
 
-# class Doctor(models.Model):
-#     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-#     clinic_id = models.ForeignKey(Clinic, on_delete=models.CASCADE)
-#     speclization = models.CharField(50)
-#     rate = models.DecimalField(3, 2)
-#     fees = models.IntegerField(5)
-#     time_slots = JSONField()
-#     description = models.CharField(1024)
+
+class User (models.Model):
+    GENDER =(
+        ('male','male'),
+        ('female','female')
+        )
+    name= models.CharField(max_length=200,null=True)
+    phone= models.CharField(max_length=200,null=True)
+    email= models.CharField(max_length=200,null=True)
+    data_created= models.DateTimeField(auto_now_add=True,null=True)
+    birth_date = models.DateTimeField(null=True)
+    gender= models.CharField(max_length=200,null=True,choices=GENDER)
+class  Patient (models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    medical_history=
+
+class Clinic (models.Model):
+    name= models.CharField(max_length=200,null=True)
+    rating = models.DecimalField(3, 2)
+    address=models.CharField(max_length=200,null=True)
+
+class Doctor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+    speclization = models.CharField(max_length=50)
+    rating = models.FloatField(null=True)
+    fees =  models.IntegerField(null=True)
+    time_slots = JSONField()
+    description = models.CharField(max_length=1024)
+
+class Appointment (models.Model):
+    patient=models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor=models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    time_slot=models.DateTimeField(null=True)
+    review=models.CharField(max_length=1024)
+
+class Appointment (models.Model):
+    key=models.CharField(max_length=250)
+    amount=models.IntegerField(null=True)
+    appointment=models.ForeignKey(Appointment, on_delete=models.SET_NULL)
+    
+
 
 
