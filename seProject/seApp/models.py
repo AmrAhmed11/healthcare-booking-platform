78b515b4da0c6b4eec9c1c6f193be9b7d19bdc07
amr1412 <amrabdulaziz98@gmail.com>
2021-01-06 02:15:22 +0200

    database moved to postgresql

--- seProject/seApp/models.py
+++ seProject/seApp/models.py
@@ -1,46 +1,55 @@
 from django.db import models
-from jsonfield import JSONField
+# from jsonfield import JSONField
 
 
-class User (models.Model):
+class User(models.Model):
     GENDER =(
         ('male','male'),
         ('female','female')
         )
-    name= models.CharField(max_length=200,null=True)
-    phone= models.CharField(max_length=200,null=True)
-    email= models.CharField(max_length=200,null=True)
-    data_created= models.DateTimeField(auto_now_add=True,null=True)
+    name = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200,null=True)
+    data_created = models.DateTimeField(auto_now_add=True,null=True)
     birth_date = models.DateTimeField(null=True)
-    gender= models.CharField(max_length=200,null=True,choices=GENDER)
-class  Patient (models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
-    medical_history=
+    gender = models.CharField(max_length=200,null=True,choices=GENDER)
+    def __str__(self):
+        return self.name
+class  Patient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # medical_history=
+    def __str__(self):
+        return self.user.name
 
-class Clinic (models.Model):
-    name= models.CharField(max_length=200,null=True)
-    rating = models.DecimalField(3, 2)
-    address=models.CharField(max_length=200,null=True)
+class Clinic(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    # rating = models.DecimalField(3, 2)
+    address = models.CharField(max_length=200,null=True)
+    def __str__(self):
+        return self.name
 
 class Doctor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
-    speclization = models.CharField(max_length=50)
+    specialization = models.CharField(max_length=50)
     rating = models.FloatField(null=True)
     fees =  models.IntegerField(null=True)
-    time_slots = JSONField()
+    # time_slots = JSONField()
     description = models.CharField(max_length=1024)
+    
+    def __str__(self):
+        return self.user.name
 
-class Appointment (models.Model):
-    patient=models.ForeignKey(Patient, on_delete=models.CASCADE)
-    doctor=models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    time_slot=models.DateTimeField(null=True)
-    review=models.CharField(max_length=1024)
-
-class Appointment (models.Model):
-    key=models.CharField(max_length=250)
-    amount=models.IntegerField(null=True)
-    appointment=models.ForeignKey(Appointment, on_delete=models.SET_NULL)
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor=  models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    time_slot = models.DateTimeField(null=True)
+    review = models.CharField(max_length=1024)
+    
+class Payment(models.Model):
+    key = models.CharField(max_length=250)
+    amount = models.IntegerField(null=True)
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
     
 
 
--- seProject/seProject/settings.py
+++ seProject/seProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'seApp',
+    'seApp'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'seProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'seProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'seapp',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
