5ca20531c3065a6c06bd233a35bd8032ee9d4ac5
amr1412 <amrabdulaziz98@gmail.com>
2021-01-06 02:36:46 +0200

    Arrays added but buggy

--- seProject/seApp/models.py
+++ seProject/seApp/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-# from jsonfield import JSONField
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(models.Model):
-    GENDER =(
-        ('male','male'),
-        ('female','female')
+    GENDER = (
+        ('Male','Male'),
+        ('Female','Female')
         )
     name = models.CharField(max_length=200,null=True)
     phone = models.CharField(max_length=200,null=True)
@@ -15,42 +15,45 @@ class User(models.Model):
     gender = models.CharField(max_length=200,null=True,choices=GENDER)
     def __str__(self):
         return self.name
-class  Patient(models.Model):
+
+
+class Patient(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # medical_history=
+    medical_history = ArrayField(models.CharField(max_length = 200, null = True), size = 1000, null= True)
     def __str__(self):
         return self.user.name
 
 class Clinic(models.Model):
-    name = models.CharField(max_length=200,null=True)
-    # rating = models.DecimalField(3, 2)
-    address = models.CharField(max_length=200,null=True)
+    name = models.CharField(max_length = 200, null = True)
+    rating = models.FloatField(null= True)
+    address = models.CharField(max_length = 200,null = True)
     def __str__(self):
         return self.name
 
+
 class Doctor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
-    specialization = models.CharField(max_length=50)
-    rating = models.FloatField(null=True)
-    fees =  models.IntegerField(null=True)
-    # time_slots = JSONField()
-    description = models.CharField(max_length=1024)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete = models.CASCADE)
+    specialization = models.CharField(max_length = 50, null= True)
+    rating = models.FloatField(null = True)
+    fees =  models.IntegerField(null = True)
+    time_slots = ArrayField(models.DateTimeField(null = True), size = 24, null= True)
+    description = models.CharField(max_length = 1024)
     
     def __str__(self):
         return self.user.name
 
+
 class Appointment(models.Model):
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    doctor=  models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    time_slot = models.DateTimeField(null=True)
-    review = models.CharField(max_length=1024)
-    
+    patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete = models.CASCADE)
+    time_slot = models.DateTimeField(null = True)
+    review = models.CharField(max_length = 1024)
+    prescription = ArrayField(models.CharField(max_length = 70, null= True) , size=10,null= True)
+
+
 class Payment(models.Model):
     key = models.CharField(max_length=250)
     amount = models.IntegerField(null=True)
-    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
-    
-
-
+    appointment = models.ForeignKey(Appointment, on_delete = models.CASCADE)
 
