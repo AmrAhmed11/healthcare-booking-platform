6a87c8bbc69074c49f1aeb259da1ffbc18e8de7d
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2021-01-06 20:41:33 +0200

    bug fixes

--- seProject/seApp/urls.py
+++ seProject/seApp/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-# from . import views
+from . import views
 
 app_name = 'seApp'
 urlpatterns = [
-    # path('doctor/appointment', views.appointmentManager, name='appointmentManager'),
-    # path('doctor/appointment/<int:app_id>', views.appointment, name='appointment'),
-    # path('doctor/staff', views.staffManager, name='staffManager'),
-    # path('doctor/services', views.servicesManager, name='servicesManager'),
+    path('doctor/appointment', views.appointmentManager, name='appointmentManager'),
+    path('doctor/appointment/<int:app_id>', views.appointment, name='appointment'),
+    path('doctor/staff', views.staffManager, name='staffManager'),
+    path('doctor/services', views.servicesManager, name='servicesManager'),
 ]
--- seProject/seApp/views.py
+++ seProject/seApp/views.py
@@ -1,25 +1,25 @@
-# from django.shortcuts import render
-# # from .models import Doctor
+from django.shortcuts import render
+# from .models import Doctor
 
-# # Create your views here.
-# def appointmentManager(request):
-#     app_list = [{'id':0, 'date':'1/1/2022', 'state': 'Pending'}, {'id':1, 'date':'1/2/2022', 'state': 'Pending'}, {'id':2, 'date':'1/1/2022', 'state': 'Pending'}, {'id':3, 'date':'1/2/2022', 'state': 'Pending'}]
-#     context = {'app_list': app_list}
-#     return render(request, 'seApp/appointmentManager.html', context)
+# Create your views here.
+def appointmentManager(request):
+    app_list = [{'id':0, 'date':'1/1/2022', 'state': 'Pending'}, {'id':1, 'date':'1/2/2022', 'state': 'Pending'}, {'id':2, 'date':'1/1/2022', 'state': 'Pending'}, {'id':3, 'date':'1/2/2022', 'state': 'Pending'}]
+    context = {'app_list': app_list}
+    return render(request, 'seApp/appointmentManager.html', context)
 
-# def appointment(request, app_id):
-#     app = {'id':4, 'date':'1/1/2022', 'state': 'Pending'}
-#     context = {'app': app}
-#     return render(request, 'seApp/appointment.html', context)
-
-
-# def servicesManager(request):
-#     services_list = {'fees':'100$', 'timeslots':[{'id':0,'slot':'2020/12/12'},{'id':1,'slot':'2020/12/12'},{'id':2,'slot':'2020/12/12'}] }
-#     context = {'services_list': services_list}
-#     return render(request, 'seApp/servicesManager.html', context)
+def appointment(request, app_id):
+    app = {'id':4, 'date':'1/1/2022', 'state': 'Pending'}
+    context = {'app': app}
+    return render(request, 'seApp/appointment.html', context)
 
 
-# def staffManager(request):
-#     staff_list = [{'id':0, 'name':'John', 'details': 'nurse'}, {'id':1, 'name':'John', 'details': 'nurse'},{'id':2, 'name':'John', 'details': 'nurse'},{'id':3, 'name':'John', 'details': 'nurse'}]
-#     context = {'staff_list': staff_list}
-#     return render(request, 'seApp/staffManager.html', context)
+def servicesManager(request):
+    services_list = {'fees':'100$', 'timeslots':[{'id':0,'slot':'2020/12/12'},{'id':1,'slot':'2020/12/12'},{'id':2,'slot':'2020/12/12'}] }
+    context = {'services_list': services_list}
+    return render(request, 'seApp/servicesManager.html', context)
+
+
+def staffManager(request):
+    staff_list = [{'id':0, 'name':'John', 'details': 'nurse'}, {'id':1, 'name':'John', 'details': 'nurse'},{'id':2, 'name':'John', 'details': 'nurse'},{'id':3, 'name':'John', 'details': 'nurse'}]
+    context = {'staff_list': staff_list}
+    return render(request, 'seApp/staffManager.html', context)
