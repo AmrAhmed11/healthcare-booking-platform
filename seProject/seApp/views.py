6822958b1986e289b5514505dac3d7356076a376
Omar Rashed <70315665+OmarRash@users.noreply.github.com>
2021-01-09 00:52:01 +0200

    import group and editing login

--- seProject/seApp/views.py
+++ seProject/seApp/views.py
@@ -13,6 +13,8 @@ from django.contrib import messages
 from django.contrib.auth import  authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import string
+from django.contrib.auth.models import Group
+
 
 
 def loginpage (request):
