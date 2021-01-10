8d72777291c685090ffb904340ab35d98285cf82
Luai Anwar <55032660+LoayAnwar@users.noreply.github.com>
2021-01-10 20:35:39 +0200

    bug fix

--- seProject/seApp/forms.py
+++ seProject/seApp/forms.py
@@ -69,7 +69,3 @@ class RateForm(ModelForm):
         model = Doctor
         fields = ['rating',]
        
-class chooseAppointmentForm(ModelForm):
-    class Meta:
-        model = Doctor
-        fields =['time_slots',]
