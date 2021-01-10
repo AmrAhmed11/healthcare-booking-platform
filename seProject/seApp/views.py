e969a31a0924e99d8bed04ea1825eb87d1dfeb0a
LailaWael3 <77001864+LailaWael3@users.noreply.github.com>
2021-01-10 19:35:33 +0200

    adding Edit

--- seProject/seApp/views.py
+++ seProject/seApp/views.py
@@ -239,12 +239,11 @@ def browse(request):
 
 def appointmentUser(request, user_id):
     patient = Patient.objects.get(id=user_id)
-    app_all= patient.appointment_set.all()
     app_pending =patient.appointment_set.filter(status="Pending")
     app_done = patient.appointment_set.filter(status="Done")
     app_cancelled = patient.appointment_set.filter(status="Cancelled")
 
-    context = {'app_pending': app_pending,'app_done': app_done,'app_cancelled': app_cancelled, 'app_all' : app_all}
+    context = {'app_pending': app_pending,'app_done': app_done,'app_cancelled': app_cancelled}
 
     return render(request, 'seApp/appointmentUser.html', context)    
 
@@ -300,11 +299,20 @@ def review(request, app_id ) :
     context = {'app': app, 'form': form, 'formrate': formrate}
     return render(request, 'seApp/review.html', context)     
 
+def cancel(request, app_id ) :
+    app = Appointment.objects.get(id=app_id)
+ 
+    
+    if request.method == 'POST':
+        app.status = "Cancelled"
+        app.save()
 
-
+    context = {'app': app}
+    return render(request, 'seApp/cancel.html', context)
+    
 def viewDoctor(request, doctor_id):
-    doctors = Doctor.objects.get(id = doctor_id)
-    patient = Patient.objects.get(id = 1)
+    doctors = Doctor.objects.get(id = 3)
+    patient = Patient.objects.get(id = 2)
     if request.method == 'POST':
         appointment = Appointment(
                 patient = patient,
@@ -317,5 +325,8 @@ def viewDoctor(request, doctor_id):
         appointment.save()
     context = {'doctors':doctors,}
     return render(request, 'seApp/viewDoctor.html', context)
-    
 
+def editappointment (request, app_id):    
+  
+    context = {}
+    return render(request, 'seApp/editappointment.html', context)     
