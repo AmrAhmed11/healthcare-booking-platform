4212685b65086948a9734e7da5c579347069c905
amr1412 <amrabdulaziz98@gmail.com>
2021-01-11 00:34:30 +0200

    clinic owner_id added and birth_date fixed

--- seProject/seApp/models.py
+++ seProject/seApp/models.py
@@ -7,18 +7,18 @@ class UserProfile(AbstractUser):
         ('Male','Male'),
         ('Female','Female')
         )
-    ROLES=(
+    ROLES = (
         ('doctor','Doctor'),
         ('patient','Patient'),
         ('staff','Staff')
     )
-    phone = models.CharField(max_length=200,null=True)
-    birth_date = models.DateTimeField(null=True)
-    gender = models.CharField(max_length=200,null=True,choices=GENDER)
-    role=models.CharField(max_length=200,null=True,choices=ROLES)
+    phone = models.CharField(max_length = 200, null = True)
+    birth_date = models.DateField(null = True)
+    gender = models.CharField(max_length = 200, null = True, choices = GENDER)
+    role = models.CharField(max_length = 200, null = True, choices = ROLES)
 
     def __str__(self):
-        return self.first_name+' '+self.last_name
+        return self.first_name + ' ' + self.last_name
 
 
 class Patient(models.Model):
@@ -29,6 +29,7 @@ class Patient(models.Model):
 
 class Clinic(models.Model):
     name = models.CharField(max_length = 200, null = True)
+    owner_id = models.IntegerField(null=True)
     rating = models.FloatField(null= True)
     address = models.CharField(max_length = 200, null = True)
     def __str__(self):
@@ -36,12 +37,12 @@ class Clinic(models.Model):
  
 
 class Doctor(models.Model):
-    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    clinic = models.ForeignKey(Clinic, on_delete = models.CASCADE,null=True)
+    user = models.OneToOneField(UserProfile, on_delete = models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete = models.CASCADE, null = True)
     specialization = models.CharField(max_length = 50, null= True)
     rating = models.FloatField(null = True)
     fees =  models.IntegerField(null = True)
-    time_slots = ArrayField(models.DateTimeField(null = True), size = 24, null= True)
+    time_slots = ArrayField(models.DateTimeField(null = True), size = 24, null = True)
     description = models.CharField(max_length = 1024, null = True)
     medical_id = models.CharField(max_length = 20, null = True)
     
@@ -54,9 +55,9 @@ class Staff(models.Model):
         ('nurse','Nurse'),
         ('lab_specialist','Lab Specialist')
         )
-    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctor, on_delete = models.CASCADE, null=True)
-    specialization = models.CharField(max_length = 20, null= True, choices = specialization_choices)
+    user = models.OneToOneField(UserProfile, on_delete = models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete = models.CASCADE, null = True)
+    specialization = models.CharField(max_length = 20, null = True, choices = specialization_choices)
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
 
@@ -68,15 +69,15 @@ class Appointment(models.Model):
 			)
     patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete = models.CASCADE)
-    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    status = models.CharField(max_length = 200, null = True, choices = STATUS)
     time_slot = models.DateTimeField(null = True)
     review = models.CharField(max_length = 1024)
-    prescription = ArrayField(models.CharField(max_length = 70, null= True) , size=10,null= True)
+    prescription = ArrayField(models.CharField(max_length = 70, null = True) , size = 10, null = True)
 	
 
 
 class Payment(models.Model):
-    key = models.CharField(max_length=250)
-    amount = models.IntegerField(null=True)
+    key = models.CharField(max_length = 250)
+    amount = models.IntegerField(null = True)
     appointment = models.ForeignKey(Appointment, on_delete = models.CASCADE)
 
