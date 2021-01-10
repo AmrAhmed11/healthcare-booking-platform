325797f950a817de21bd456ec47867cfa0d5f104
AmrAhmed11 <57914179+AmrAhmed11@users.noreply.github.com>
2021-01-10 22:54:43 +0200

    spaces

--- seProject/seApp/views.py
+++ seProject/seApp/views.py
@@ -337,23 +337,23 @@ def cancel(request, app_id ) :
 def viewDoctor(request, doctor_id):
     doctors = Doctor.objects.get(id=doctor_id)
     if request.method == 'POST':
-       if request.user.is_authenticated:
+        if request.user.is_authenticated:
             patient = Patient.objects.get(id=request.user.patient.id)
             timeslots = doctors.time_slots
             timeslot = timeslots[int(request.POST['appointment']) - 1]
             doctors.time_slots.remove(timeslot)
-        appointment = Appointment(
-                patient = patient,
-                doctor = doctors,
-                status = 'Pending',
-                time_slot = timeslot,
-                review = 'None',
-                prescription = []
-        )
-        appointment.save()
-        doctors.save()
+            appointment = Appointment(
+            patient = patient,
+            doctor = doctors,
+            status = 'Pending',
+            time_slot = timeslot,
+            review = 'None',
+            prescription = []
+            )
+            appointment.save()
+            doctors.save()
         else:
-        return render(request, 'seApp/login.html')
+            return render(request, 'seApp/login.html')
     context = {'doctors':doctors,}
     return render(request, 'seApp/viewDoctor.html', context)
 
