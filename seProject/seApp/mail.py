cdec8b08691acc076188b6452b3abff2c250f60e
Loayelshall <Loay.elshall@icloud.com>
2021-01-12 00:05:41 +0200

    mail bug fix

--- seProject/seApp/mail.py
+++ seProject/seApp/mail.py
@@ -1,16 +1,15 @@
-from django.core.mail import EmailMultiAlternatives
+# from django.core.mail import EmailMultiAlternatives
+from django.core import mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.template import RequestContext
 
 
 # THIS IS AN EXAMPLE FOR SENDING EMAILS
-
-subject = 'Test'
-from_email = 'donotreply@seapp.com'
-to = 'loay.elshall@icloud.com'
-text_content = 'This is a text message.'
-html_content = '
-This is an HTML message.
-
-'
-msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-msg.attach_alternative(html_content, "text/html")
-msg.send(fail_silently=False)
+    subject = 'Test'
+    from_email = 'seApp <1701043@eng.asu.edu.eg>'
+    to = 'loay.elshall@icloud.com'
+    paragraph = "alo alo alo"
+    html_message = render_to_string('seApp/mail_template.html', {'context': paragraph})
+    plain_message = strip_tags(html_message)
+    mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
