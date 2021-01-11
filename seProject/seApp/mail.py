83691bac7315edea84674cc4db1239347ea40ba6
Loayelshall <Loay.elshall@icloud.com>
2021-01-12 01:05:29 +0200

    mail finish

--- seProject/seApp/mail.py
+++ seProject/seApp/mail.py
@@ -2,14 +2,15 @@
 from django.core import mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from django.template import RequestContext
+
 
 
 # THIS IS AN EXAMPLE FOR SENDING EMAILS
+def sendEmail(subject,from_email,to,mail_template)
     subject = 'Test'
     from_email = 'seApp <1701043@eng.asu.edu.eg>'
     to = 'loay.elshall@icloud.com'
     paragraph = "alo alo alo"
-    html_message = render_to_string('seApp/mail_template.html', {'context': paragraph})
+    html_message = render_to_string('seApp/mail_temps/' + mail_template + '.html', {'context': paragraph})
     plain_message = strip_tags(html_message)
     mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
