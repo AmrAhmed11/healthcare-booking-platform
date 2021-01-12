f2d28e01325ac6ff6dbb827bd015b56b88dcad27
Loayelshall <Loay.elshall@icloud.com>
2021-01-12 02:19:59 +0200

    mail bug fix

--- seProject/seApp/mail.py
+++ seProject/seApp/mail.py
@@ -6,11 +6,9 @@ from django.utils.html import strip_tags
 
 
 # THIS IS AN EXAMPLE FOR SENDING EMAILS
-def sendEmail(subject,from_email,to,mail_template)
-    subject = 'Test'
+def sendEmail(subject,to,mail_template):
     from_email = 'seApp <1701043@eng.asu.edu.eg>'
-    to = 'loay.elshall@icloud.com'
-    paragraph = "alo alo alo"
+    paragraph = "test"
     html_message = render_to_string('seApp/mail_temps/' + mail_template + '.html', {'context': paragraph})
     plain_message = strip_tags(html_message)
     mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
